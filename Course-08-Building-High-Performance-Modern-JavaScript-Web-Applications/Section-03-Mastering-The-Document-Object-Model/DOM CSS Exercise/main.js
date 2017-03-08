72778ea741cc58d497556f7457393983b48993b4
Imanuel Gittens <gittensimanuel@gmail.com>
2017-03-08 12:32:05 -0400

    Continue working on javascript stylesheets exercise.

--- Course-08-Building-High-Performance-Modern-JavaScript-Web-Applications/Section-03-Mastering-The-Document-Object-Model/DOM CSS Exercise/main.js
+++ Course-08-Building-High-Performance-Modern-JavaScript-Web-Applications/Section-03-Mastering-The-Document-Object-Model/DOM CSS Exercise/main.js
@@ -1,10 +1,18 @@
 var stylesheets = document.styleSheets;
-
+/* Deleting a CSS rule */
 for (var x = 0; x < stylesheets.length; x++) {
     for(var y = 0; y < stylesheets[x].cssRules.length; y++){
+        if(stylesheets[x].cssRules[y].selectorText === '.menu'){
+            stylesheets[x].deleteRule(y);
+        }
         console.log(stylesheets[x].cssRules[y]);
     }
-
-
-
 }
+
+/* Creating a new stylesheet object */
+
+var style = document.createElement('style');
+var styleSheet = style.styleSheet;
+style.insertRule('.post {width: 100%}', styleSheet.cssRules.length);
+styleSheet.insertRule('.post__title) {font-size: 40px}', styleSheet.cssRules.length);
+console.log(styleSheet);
